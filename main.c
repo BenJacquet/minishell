c73972345f2f848a53b8eb5d775bececd5be715a
Charles-ambroise GILBERT <chgilber@e1r10p4.42.fr>
2020-08-04 18:36:43 +0200

    end,cd,main{maj}

--- main.c
+++ main.c
@@ -6,66 +6,48 @@
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/04 14:09:45 by chgilber          #+#    #+#             */
-/*   Updated: 2020/08/04 16:58:43 by chgilber         ###   ########.fr       */
+/*   Updated: 2020/08/04 18:35:34 by chgilber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include "GNL/get_next_line.h"
-#include "libft/libft.h"
+#include "minishell.h"
 
-int get_dir()
+int		check(char	*buff)
 {
-    char *path;
-    char *dir;
-    void *bkp;
-    int len;
-
-    path = getcwd(path, 0);
-    bkp = path;
-    while (*path != '\0')
-    {
-        if (*path == '/')
-            dir = ++path;
-        path++;
-    }
-    len = strlen(dir);
-    write(1, dir, len);
-    free(bkp);
-    return (0);
+	//	printf("%d,%s\n", ft_strlen(buff), buff);
+	if (ft_strlen(buff) == 4 && ft_strncmp(buff, "exit", 4) == 0)
+	{
+		write(1, "exit\n", 5);
+		return (0);
+	}
+	if (ft_strlen(buff) > 4 && ft_strncmp(buff, "exit ", 5) == 0)
+	{
+		write(1, "exit\n", 5);
+		return (end(buff));
+	}
+	else
+		return (1);
 }
-
 int		main(int ac, char **av)
 {
-	int	i;
-	int len;
-	int lenw;
-	char *pwd;
-	char **dir;
-	char *buff;
+	int		i;
+	char	**dir;
+	char	*buff;
 
-    get_dir();
-	write(1, " -> :", 5);
-	get_next_line(0,&buff);
-	while(strcmp(buff,"exit") != 0)
+	i = 0;
+	get_dir();
+	get_next_line(0, &buff);
+	while(check(buff) == 1)
 	{
-	dir = ft_split(buff, ' ');
-	if(ft_strncmp(dir[0], "cd", 2) == 0)
-		i = chdir((dir[1]));
-	else
-		system(buff);
-    get_dir();
-	write(1, " -> :", 5);
-	free(buff);
-	get_next_line(0,&buff);
+		dir = ft_split(buff, ' ');
+		if(ft_strncmp(dir[0], "cd", 2) == 0)
+			cd(dir);
+		else
+			system(buff);
+		get_dir();
+		free(buff);
+		get_next_line(0,&buff);
 	}
 	free(buff);
-	printf("%s", pwd + len);
-	write(1, "lectured", 8);
-	return(i);
+	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,9 +6,21 @@
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/04 16:18:54 by jabenjam          #+#    #+#             */
-/*   Updated: 2020/08/04 16:23:08 by jabenjam         ###   ########.fr       */
+/*   Updated: 2020/08/04 17:59:39 by chgilber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include "GNL/get_next_line.h"
+#include "libft/libft.h"
 
 /*
 **------OLD FUNCTIONS-------------------------------------------------------------
@@ -19,5 +31,7 @@ int get_dir();
 /*
 **------NEW FUNCTIONS-------------------------------------------------------------
 */
-
+int		end(char	*buff);
+int		cd(char		**dir);
+#endif
 
