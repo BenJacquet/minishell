f0cd5cfe7da6a7626bce13d293f0b2dbea69b8bb
Jacquet BENJAMIN <jabenjam@e1r10p2.42.fr>
2020-08-07 14:43:37 +0200

    modified main

--- main.c
+++ main.c
@@ -6,28 +6,11 @@
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/04 14:09:45 by chgilber          #+#    #+#             */
-/*   Updated: 2020/08/07 14:32:21 by jabenjam         ###   ########.fr       */
+/*   Updated: 2020/08/07 14:43:15 by jabenjam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int check(char *buff)
-{
-	//	printf("%d,%s\n", ft_strlen(buff), buff);
-	if (ft_strlen(buff) == 4 && ft_strncmp(buff, "exit", 4) == 0)
-	{
-		write(1, "exit\n", 5);
-		return (0);
-	}
-	if (ft_strlen(buff) > 4 && ft_strncmp(buff, "exit ", 5) == 0)
-	{
-		write(1, "exit\n", 5);
-		return (end(buff));
-	}
-	else
-		return (1);
-}
 
 int main(int ac, char **av, char **env)
 {
