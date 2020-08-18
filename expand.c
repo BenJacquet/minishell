79aa22534cf4bf43007a6fc12e72f5a732ee81d1
Jacquet BENJAMIN <jabenjam@e1r8p4.42.fr>
2020-08-18 18:19:09 +0200

    added expand token

--- expand.c
+++ expand.c
@@ -6,11 +6,22 @@
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 17:01:11 by jabenjam          #+#    #+#             */
-/*   Updated: 2020/08/18 18:02:58 by jabenjam         ###   ########.fr       */
+/*   Updated: 2020/08/18 18:19:01 by jabenjam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int env_exists(char *var, t_env *env)
+{
+    while (env)
+    {
+        if (ft_strcmp(var, env->name) == 0)
+            return (1);
+        env = env->next;
+    }
+    return (0);
+}
 
 int     expand_token(char **token, t_all *all)
 {
@@ -25,9 +36,11 @@ int     expand_token(char **token, t_all *all)
             return (1);
         else
         {
-            if (ft_strlen(*token) >= 2)
-            *token = ft_getenv(*token + 1, ft_list_to_tab(all->env));
-            return (2);
+            if (ft_strlen(*token) >= 2 && env_exists(*token + 1, all->env))
+            {
+                *token = ft_getenv(*token + 1, ft_list_to_tab(all->env));
+                return (2);
+            }
         }
     }
     return (0);
