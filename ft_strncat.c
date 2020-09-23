66deb887d9ab3ba41f86700f19577e26fd655eec
Charles-ambroise GILBERT <chgilber@e1r2p2.42.fr>
2020-09-23 15:01:23 +0200

    files bro

---
+++ ft_strncat.c
@@ -0,0 +1,21 @@
+
+#include "minishell.h"
+
+char	*ft_strncat(char *dest, const char *src, size_t dstsize)
+{
+	size_t		i;
+	size_t		o;
+
+	o = 0;
+	i = 0;
+	while (dest[i])
+		i++;
+	while (src[o] && o < dstsize)
+	{
+		dest[i + o] = src[o];
+		o++;
+	}
+	if (o != dstsize)
+		dest[i + o] = '\0';
+	return (dest);
+}
