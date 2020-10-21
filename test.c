efd6a0532d3f30652d68e723cb544fba32970494
Jacquet BENJAMIN <jabenjam@e1r3p16.42.fr>
2020-10-21 18:03:13 +0200

    pipes implementation test IS A FUCKING SUCCESS !!!

--- test.c
+++ test.c
@@ -6,53 +6,106 @@
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 15:31:57 by jabenjam          #+#    #+#             */
-/*   Updated: 2020/10/16 15:10:20 by jabenjam         ###   ########.fr       */
+/*   Updated: 2020/10/21 18:02:02 by jabenjam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		pipes_manager2(t_all *all, char **execs, int pipes, char **envp)
+void	restore_fds(int in, int out)
 {
-	int		status;
-	pid_t	pid;
-	int		ret;
-	int		fds[pipes][2];
+	dup2(in, STDIN_FILENO);
+	close(in);
+	dup2(out, STDOUT_FILENO);
+	close(out);
+}
 
-	status = 0;
-	ret = 0;
-	for (int i = 0; i < pipes ; i++)
-		pipe(fds[i]);
-	for (int i = 0;all->cmds[i];i++)
+void	manage_pipes_2(int fd[3][2], int around, int num)
+{
+	if (around == 0)
+	{
+		close(fd[num - 1][0]);
+	}
+	if (around == 1)
+	{
+		close(fd[num][1]);
+	}
+	if (around == 2)
+	{
+		close(fd[num - 1][0]);
+		close(fd[num][1]);
+	}
+}
+
+void	manage_pipes_1(int fd[2][2], int around, int num)
+{
+	if (around == 2)
+	{
+		dup2(fd[num - 1][0], STDIN_FILENO);
+		dup2(fd[num][1], STDOUT_FILENO);
+	}
+	else if (around == 0)
+	{
+		dup2(fd[num - 1][0], STDIN_FILENO);
+	}
+	else if (around == 1)
+	{
+		dup2(fd[num][1], STDOUT_FILENO);
+	}
+	for (int j = 0; j < 2 ; j++)
+	{
+		close(fd[j][1]);
+		close(fd[j][0]);
+	}
+}
+
+int		main(int ac, char **av, char **env)
+{
+	int		pip[2][2];
+	int		i = 0;
+	int		pid = 0;
+	(void)ac;
+	(void)av;
+	char **cmd1 = ft_split("/bin/ls",' ');
+	char **cmd2 = ft_split("/bin/cat",' ');
+	char **cmd3 = ft_split("/usr/bin/grep main",' ');
+	char ***cmds;
+
+	cmds = malloc(sizeof(char**) * 4);
+	cmds[0] = cmd1;
+	cmds[1] = cmd2;
+	cmds[2] = cmd3;
+	cmds[3] = NULL;
+
+	int around[3];
+
+	around[0] = 1;
+	around[1] = 2;
+	around[2] = 0;
+
+	int in = dup(STDIN_FILENO);
+	int out = dup(STDOUT_FILENO);
+	for (int j = 0; j < 2; j++)
+	{
+		printf("opened pipe number %d\n", j);
+		pipe(pip[j]);
+	}
+	while (cmds[i])
 	{
 		if ((pid = fork()) == 0)
 		{
-			status = execve(execs[i], all->cmds[i], envp);
-			if (status == -1)
-				ft_put_error(strerror(errno), execs[i], 1);
+			manage_pipes_1(pip, around[i], i);
+			write(2, "exec: ", 6);
+			write(2, cmds[i][0], strlen(cmds[i][0]));
+			write(2, "\n", 1);
+			execve(cmds[i][0], cmds[i], env);
+			exit(1);
 		}
 		else
-			waitpid(pid, &ret, 0);
+		{
+			waitpid(pid, NULL, 0);
+			manage_pipes_2(pip, around[i], i);
+		}
+		i++;
 	}
-	update_return(all, (ret / 256));
-	free(envp);
-	return (0);
-}
-
-int		pipes_manager(t_all *all)
-{
-	int		pipes = ttablen(all->cmds);
-	char	**execs;
-
-	printf("pipes=[%d]\n", pipes);
-
-	execs = malloc(sizeof(char *) * (pipes + 1));
-	for (int i = 0;all->cmds[i]; i++)
-	{
-		all->dir = all->cmds[i];
-		execs[i] = get_path(all);
-		printf("execs[%d]=[%s]\n", i, execs[i]);
-	}
-	pipes_manager2(all, execs, pipes, ft_list_to_tab(all->env, 0, 0));
-	exit(0);
 }
