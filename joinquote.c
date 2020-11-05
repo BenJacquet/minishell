0ea8e37c7d4a98fcf3ac0c22430e226b8a41de2d
charles <charleambg@gmail.com>
2020-11-05 15:17:00 +0100

    gestion $$

--- joinquote.c
+++ joinquote.c
@@ -14,8 +14,8 @@
 
 char	**newdirquote(char **dir, int count)
 {
-	dir = malloc(sizeof(char *) * (count + 1));
-	dir[count] = NULL;
+	dir = malloc(sizeof(char *) * (count + 2));
+//	dir[count] = NULL;
 	return (dir);
 }
 
@@ -24,13 +24,14 @@ int		noquote(t_all *all, char *buff, int *inc)
 	int	i;
 	int fusion;
 
-	fusion = (*inc > 0 && (buff[0] == '\'' || buff[0] == '\"')
+	fusion = (*inc > 0 && (buff[0] != ' ')//(buff[0] == '\'' || buff[0] == '\"')
 			&& (buff[1] != '<' && buff[1] != '>')) ? 1 : 0;
 	i = (buff[1] == ' ') ? 2 : 1;
+//	printf("noquote buff before = [%s]\n", buff + ((buff[1] == ' ') ? 2 : 1));
 	while (buff[i] && buff[i] != '\'' && buff[i] != ' ' && buff[i] != '\"')
 		i++;
 	buff = tokla(all, buff, &i, (buff[1] == ' ') ? 0 : 1);
-//	printf("i = %d\n", i);
+//	printf("i = %d fusion = %d\n", i, fusion);
 //	printf("noquote buff = [%s]\n", buff + ((buff[1] == ' ') ? 2 : 1));
 //	printf("noquote pdir = [%s]\n",all->pdir[all->data - all->countsmc]);
 	if (fusion == 0 && ((buff[1] == ' ') ? i > 2 : i > 1))
@@ -58,17 +59,17 @@ int		ifjoin(t_all *all, char *buff, int *inc, char quote)
 	i = 2;
 	while (buff[i] != quote)
 		i++;
-	//printf("ifjoin buff avant = [%s] et %d\n", buff  + 2, i );
+//	printf("ifjoin buff avant = [%s] et %d\n", buff  + 2, i );
 	croco(all, buff, i, *inc);
-	buff = tokla(all, buff, &i, 0);
-//	printf("ifjoin buff = [%s] et %d\n", buff  + 2, i );
+//	buff = tokla(all, buff, &i, 0);
+	printf("ifjoin buff = [%s] et %d\n", buff  + 2, i );
 	if (*inc > 0 && (buff[0] == ' ' || buff[0] == '<' || buff[0] == '>')
 			&& buff[2] != quote)
 	{
 		all->dir[*inc] = malloc(sizeof(char) * i);
 		all->dir[*inc] = ft_strncpy(all->dir[*inc], buff + 2, i - 2);
 	}
-	else if (*inc > 0 && i != 2)
+	else if (*inc > 0 && buff[0] != ' ')//i != 2)
 		joinjoin(all, buff, inc, i);
 	else
 	{
--- token.c
+++ token.c
@@ -29,20 +29,27 @@ void	finddolar(t_all *all, int *i, char **tmp)
 	}
 	tmp[1][all->j] = '\0';
 	tmp[2] = ft_getenv(all, tmp[1], (all->kotey == 2) ? 0 : 1);
+	if (all->j == 0)
+	{
+		free(tmp[2]);
+		tmp[2] = ft_strdup("$");
+	}
 	all->diff = ft_strlen(tmp[2]);
 }
 
 int		nicedolbro(t_all *all, int i, char **tmp)
 {
+	int j;
+
 	finddolar(all, &i, tmp);
-	all->j = 0;
+	j = 0;
 	while (i <= ft_strlen(tmp[0]) && tmp[0][i])
 	{
-		tmp[1][all->j] = tmp[0][i];
+		tmp[1][j] = tmp[0][i];
 		i++;
-		all->j++;
+		j++;
 	}
-	tmp[1][all->j] = '\0';
+	tmp[1][j] = '\0';
 	tmp[0] = ft_strjoin(tmp[2], tmp[1]);
 	free(tmp[2]);
 	free(tmp[1]);
@@ -56,17 +63,17 @@ void	dolar(t_all *all, char *buff, int u)
 
 	i = 0;
 	all->diff = 0;
-//	printf("pdirinit = %s\n", all->pdir[all->data - all->countsmc]);
+	printf("pdirinit = %s\n", all->pdir[all->data - all->countsmc]);
 	tmp[3] = malloc(sizeof(char) * (u + all->u + 1));
 	tmp[3] = ft_strncpy(tmp[3], all->pdir[all->data - all->countsmc], (all->u + u - 1));
 	tmp[0] = buff;
-//	printf("buff = %s\n", tmp[0]);
-//	printf("buff before dolar = %s\n", tmp[3]);
+	printf("buff = %s\n", tmp[0]);
+	printf("buff before dolar = %s\n", tmp[3]);
 	all->pipe = (all->kotey == 2) ? 1 : 0;
 	nicedolbro(all, i, tmp);
 	free(all->pdir[all->data - all->countsmc]);
 	all->pdir[all->data - all->countsmc] = ft_strjoin(tmp[3], tmp[0]);
-//	printf("buff final = %s\n", all->pdir[all->data - all->countsmc]);
+	printf("buff final = %s\n", all->pdir[all->data - all->countsmc]);
 	free(tmp[3]);
 	free(tmp[0]);
 }
@@ -88,6 +95,7 @@ char	*tokla(t_all *all, char *buff, int *end, int mode)
 			//	printf("allu = %d et i = %d et len = %d\n" , all->u, i, ft_strlen(all->pdir[all->data -all->countsmc]));
 			//	printf("tokla buff = [%s]\n", buff);
 			//	printf("tokla pdir = [%s]\n",all->pdir[all->data - all->countsmc]);
+			i = (all->diff < 1) ? i - 1 : i;	
 			all->kotey = 0;
 			*end = all->diff + i;
 			return (buff);
